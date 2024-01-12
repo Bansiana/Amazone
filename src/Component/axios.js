abfb723ea2f70994410f1a6e5ce4f8839590d94b
Bansiana <147217477+Bansiana@users.noreply.github.com>
2024-01-12 01:13:22 -0500

    commit

--- src/Component/axios.js
+++ src/Component/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:5001/me-7fd6c/us-central1/api",
+  // baseURL: "http://127.0.0.1:5001/me-7fd6c/us-central1/api",
+  // baseURL:"http://127.0.0.1:5000"
+  baseURL: "https://amazon-backend-otpu.onrender.com/",
 });
 
 export default instance;
